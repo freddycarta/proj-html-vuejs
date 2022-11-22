9f9f93d4fabb6edebfc24e2d8209667bc0355550
freddycarta <cartafederico92@gmail.com>
2022-11-22 16:14:19 +0100

    cardscomponent

--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,66 +1,61 @@
 <template>
-    <div class="container d-flex">
-        <div class="card-columns">
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
+    
+    <div class="container">
+        <h1 class="text-center">Latest Band NEWS</h1>
+        <p></p>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+                <div class="card">
+                <img src="../assets/img/blog_music_techo-400x200.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title that wraps to a new line</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
                 </div>
             </div>
-            <div class="card p-3">
-                <blockquote class="blockquote mb-0 card-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                <footer class="blockquote-footer">
-                    <small class="text-muted">
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                    </small>
-                </footer>
-                </blockquote>
-            </div>
-            <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="col">
+                <div class="card">
+                <img src="../assets/img/blog-post4.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
                 </div>
             </div>
-            <div class="card bg-primary text-white text-center p-3">
-                <blockquote class="blockquote mb-0">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-                <footer class="blockquote-footer">
-                    <small>
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                    </small>
-                </footer>
-                </blockquote>
-            </div>
-            <div class="card text-center">
+            <div class="col">
+                <div class="card">
+                <img src="../assets/img/blog-post2.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
                 </div>
             </div>
-            <div class="card">
-                <img class="card-img" src="..." alt="Card image">
-            </div>
-            <div class="card p-3 text-right">
-                <blockquote class="blockquote mb-0">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                <footer class="blockquote-footer">
-                    <small class="text-muted">
-                    Someone famous in <cite title="Source Title">Source Title</cite>
-                    </small>
-                </footer>
-                </blockquote>
-            </div>
-            <div class="card">
+            <div class="col">
+                <div class="card">
+                <img src="../assets/img/blog_flavor_rock-200x100.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="../assets/img/blog-post1-200x300.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
                 </div>
             </div>
         </div>
@@ -82,6 +77,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+ 
 
 
 </style>
